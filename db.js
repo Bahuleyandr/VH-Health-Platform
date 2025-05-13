38b93e3a50fccf1e5fc13066411a85c0045bd9c0
Bahuleyan <BahuleyanS@pm.me>
2025-05-13 08:45:22 +0530

    Switch db.js to use Render DATABASE_URL with SSL

--- db.js
+++ db.js
@@ -1,12 +1,8 @@
-// db.js
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',        // Default PostgreSQL username
-  host: 'localhost',       // Since PostgreSQL runs locally
-  database: 'vh_health',   // Your database name
-  password: 'Neurosurgeon89', // Replace with your password
-  port: 5432,              // Default PostgreSQL port
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
