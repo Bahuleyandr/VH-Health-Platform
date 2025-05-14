7310191cb431f53490faac6923380b3949d6ce81
Bahuleyan <BahuleyanS@pm.me>
2025-05-14 14:56:33 +0530

    Fix: Adjust Sentry integrations import for compatibility with Sentry v8

--- instrument.js
+++ instrument.js
@@ -1,11 +1,11 @@
 const Sentry = require('@sentry/node');
-const Tracing = require('@sentry/tracing');
+const { Integrations } = require('@sentry/tracing');
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   tracesSampleRate: 1.0,
   integrations: [
-    new Sentry.Integrations.Http({ tracing: true }),
+    new Integrations.Http({ tracing: true }),
   ],
 });
 
