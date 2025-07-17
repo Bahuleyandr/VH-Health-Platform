b5f72975487a88817d3a54d59a9baead9ac21979
Bahuleyandr <209914980+Bahuleyandr@users.noreply.github.com>
2025-07-17 18:18:30 +0530

    babel issues

--- babel.config.mjs
+++ babel.config.mjs
@@ -10,7 +10,10 @@ export default {
   sourceMaps: true,
   retainLines: true,
   plugins: [
-    '@babel/plugin-transform-optional-chaining'
+    '@babel/plugin-transform-optional-chaining',
+    '@babel/plugin-transform-nullish-coalescing-operator',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-private-methods'
   ],
   env: {
     development: {
