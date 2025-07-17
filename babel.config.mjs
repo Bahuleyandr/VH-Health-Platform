5665fe5c85876879c6ac0ea2e17aa4033ed81270
Bahuleyandr <209914980+Bahuleyandr@users.noreply.github.com>
2025-07-17 18:24:23 +0530

    babling around

--- babel.config.mjs
+++ babel.config.mjs
@@ -1,26 +1,25 @@
-// babel.config.mjs
 export default {
   presets: [
     ['@babel/preset-env', {
       targets: {
-        node: '18'
-      }
-    }]
+        node: '18',
+      },
+      useBuiltIns: 'entry',
+      corejs: 3,
+    }],
   ],
-  sourceMaps: true,
-  retainLines: true,
   plugins: [
     '@babel/plugin-transform-optional-chaining',
     '@babel/plugin-transform-nullish-coalescing-operator',
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-private-methods'
   ],
+  sourceMaps: true,
+  retainLines: true,
   env: {
     development: {
-      sourceMaps: 'inline'
+      sourceMaps: 'inline',
     },
     production: {
-      sourceMaps: true
-    }
-  }
+      sourceMaps: true,
+    },
+  },
 };
