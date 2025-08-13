b733c9f72b0586a4092657d1bb71591699680eb8
Bahuleyandr <209914980+Bahuleyandr@users.noreply.github.com>
2025-08-13 14:46:13 +0530

    stupid typos

---
+++ src/routes/admin/services/index.js
@@ -0,0 +1,7 @@
+// src/routes/admin/services/index.js
+export * from './common.js';
+export * from './statsService.js';
+export * from './activityService.js';
+export * from './alertsService.js';
+export * from './healthService.js';
+export * from './reportService.js';
