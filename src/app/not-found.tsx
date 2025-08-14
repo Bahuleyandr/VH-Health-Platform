a8dc8bbcfb712c29ede1d36700ab1224b93133bc
Bahuleyandr <209914980+Bahuleyandr@users.noreply.github.com>
2025-08-14 19:49:48 +0530

    not found error

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+// app/not-found.tsx
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you're looking for doesn't exist.</p>
+      <a href="/">Go back home</a>
+    </div>
+  );
+}
