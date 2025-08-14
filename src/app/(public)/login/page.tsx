1e470ab82e7b516cba364bb8e3cc3396afb94392
Bahuleyandr <209914980+Bahuleyandr@users.noreply.github.com>
2025-08-14 21:04:29 +0530

    meh

---
+++ src/app/(public)/login/page.tsx
@@ -0,0 +1,9 @@
+// src/app/(public)/login/page.tsx
+
+import dynamic from 'next/dynamic';
+
+const LoginClient = dynamic(() => import('./LoginClient'), { ssr: false });
+
+export default function Page() {
+  return <LoginClient />;
+}
