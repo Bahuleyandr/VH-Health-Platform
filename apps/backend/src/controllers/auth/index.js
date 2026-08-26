b66a8201847711108bf6fbda32445ae6074ddd4b
Bahuleyandr <209914980+Bahuleyandr@users.noreply.github.com>
2026-08-26 14:33:31 +0530

    chore(backend): retire dangerous unreachable audit surfaces

--- apps/backend/src/controllers/auth/index.js
+++
@@ -1,8 +0,0 @@
-// controllers/auth/index.js
-
-export * as authController from './authController.js';
-export * as firebaseAuthController from './firebaseAuthController.js';
-export * as otpController from './otpController.js';
-export * as adminAuthController from './adminAuthController.js';
-export * as staffAuthController from './staffAuthController.js';
-export * as adminOtpController from './adminOtpController.js';
